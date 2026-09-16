f171e820209af29ff73217a385239961878efd41
mohamedsadek12 <sadekmohamed973@gmail.com>
2026-09-16 11:59:34 +0300

    testing the Optimizers

--- src/DeepNN.cpp
+++ src/DeepNN.cpp
@@ -7,10 +7,63 @@
 using namespace std;
 using namespace std::chrono;
 
+
+// Test one optimizer
+void testOptimizer(const string& optimizerName, OptimizerType optimizerType, double learningRate,
+    const Matrix& X, const Matrix& y, size_t epochs)
+{
+    cout << "\n========================================\n";
+    cout << "Testing: " << optimizerName << "\n";
+    cout << "========================================\n";
+
+    NeuralNetwork nn;
+
+    nn.addLayer(2, 4, ActivationType::Tanh);
+    nn.addLayer(4, 1, ActivationType::Sigmoid);
+    
+    nn.setOptimizer(optimizerType, learningRate);
+
+    auto start = high_resolution_clock::now();
+    nn.train(X, y, epochs, learningRate);
+    auto end = high_resolution_clock::now();
+    double trainingTime = duration<double>(end - start).count();
+
+    Matrix predictions = nn.predict(X);
+
+    size_t correct = 0;
+
+
+    for (size_t i = 0; i < y.cols(); ++i)
+    {
+        if (predictions(0, i) == y(0, i))
+        {
+            ++correct;
+        }
+    }
+
+
+    double accuracy = 100.0 * static_cast<double>(correct) / static_cast<double>(y.cols());
+
+    cout << "\nOptimizer: " << optimizerName << "\n";
+
+    cout << "Training Time: " << trainingTime << " seconds\n";
+
+
+    cout << "Predictions: ";
+    for (size_t i = 0; i < predictions.cols(); ++i)
+    {
+        cout << static_cast<int>(predictions(0, i)) << " ";
+    }
+
+    cout << "\nAccuracy: " << accuracy << "%\n";
+}
+
+
 int main()
 {
     // XOR
     Matrix X(2, 4);
+
 
     X(0, 0) = 0.0;
     X(1, 0) = 0.0;
@@ -24,6 +77,8 @@ int main()
     X(0, 3) = 1.0;
     X(1, 3) = 1.0;
 
+
+    // Target values
     Matrix y(1, 4);
 
     y(0, 0) = 0.0;
@@ -31,54 +86,16 @@ int main()
     y(0, 2) = 1.0;
     y(0, 3) = 0.0;
 
-    
-    NeuralNetwork nn;
-
-    nn.addLayer(2, 4, ActivationType::Tanh);
-    nn.addLayer(4, 1, ActivationType::Sigmoid);
 
     const size_t epochs = 10000;
-    const double learningRate = 0.1;
 
-    // Training
-    auto start = high_resolution_clock::now();
+    testOptimizer("SGD", OptimizerType::SGD, 0.1, X, y, epochs);
+    testOptimizer("Momentum", OptimizerType::Momentum, 0.1, X, y, epochs);
+    testOptimizer("Adam", OptimizerType::Adam, 0.01, X, y, epochs);
 
-    nn.train(X, y, epochs, learningRate);
-
-    auto end = high_resolution_clock::now();
-
-    double trainingTime = duration<double>(end - start).count();
-
-    // Prediction
-    Matrix predictions = nn.predict(X);
-
-    size_t correct = 0;
-
-    for (size_t i = 0; i < y.cols(); ++i)
-    {
-        if (predictions(0, i) == y(0, i))
-        {
-            ++correct;
-        }
-    }
-
-    double accuracy = 100.0 * static_cast<double>(correct) / static_cast<double>(y.cols());
-
-
-    cout << "\n================\n";
-    cout << "C++ Neural Network\n";
-    cout << "==================\n";
-
-    cout << "Training Time: " << trainingTime << " seconds\n";
-
-    cout << "\nPredictions:\n";
-
-    for (size_t i = 0; i < predictions.cols(); ++i)
-    {
-        cout << static_cast<int>(predictions(0, i)) << " ";
-    }
-
-    cout << "\nAccuracy: " << accuracy << endl;
+    cout << "\n========================================\n";
+    cout << "All optimizers tested successfully.\n";
+    cout << "========================================\n";
 
     return 0;
 }
